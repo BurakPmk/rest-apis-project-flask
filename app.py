eab73e0154f06b0a7d022437e73631eac107012b
BurakPmk <burakpamuk1999@gmail.com>
2023-04-19 17:25:57 +0300

    Add model changes and migrations, make sure migrations run beafore app starts

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask,jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 from db import db
 from blocklist import BLOCKLIST
@@ -18,6 +19,7 @@ from resources.user import blp as UserBlueprint
  
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
 
     app.config["PROPAGATE_EXCEPTIONS"] = True # if there is an exception inside propagete it to main so we can see it
     app.config["API_TITLE"] = "Stores REST API" #doc title
